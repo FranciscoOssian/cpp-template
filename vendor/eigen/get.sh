e4cd8b01c6ffd56f2922bb67484fceea5713670f
FranciscoOssian <ossian.neto@gmail.com>
2024-05-07 17:10:41 -0300

    gets

---
+++ vendor/eigen/get.sh
@@ -0,0 +1,7 @@
+cd vendor/eigen
+
+wget https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz -O eigen.tar.gz 
+
+tar -zxvf eigen.tar.gz --strip-components=1
+
+cd ../..
---
+++ vendor/viennaCL/get.sh
@@ -0,0 +1,7 @@
+cd vendor/viennaCL
+
+wget https://sourceforge.net/projects/viennacl/files/1.7.x/ViennaCL-1.7.0.tar.gz/download -O viennaCL.tar.gz 
+
+tar -zxvf viennaCL.tar.gz --strip-components=1
+
+cd ../..
